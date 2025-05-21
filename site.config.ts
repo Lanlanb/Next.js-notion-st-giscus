35ff883e75188950ec85f3fac3f4f7f98781333e
Bulan Nurlaela <bulannurlaela.72110176.9@gmail.com>
2025-05-21 21:11:09 +0700

    Update site.config.ts
    Navigation id page

--- site.config.ts
+++ site.config.ts
@@ -50,18 +50,26 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default',
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default',
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Proyek',
+      pageId: '151c813f9c5480ccb0aed42d5dd21ac0'
+    },
+    {
+      title: 'Blog',
+      pageId: '13dc813f9c5481df8f42d69cb76d38d8'
+    },
+    {
+      title: 'About',
+      pageId: '13dc813f9c5481d8b258c10555e24785'
+    },
+    {
+      title: 'Get in Touch',
+      pageId: '140c813f9c5480c49e9ad6700cbb75b9'
+    }
+  ]
 
   giscus: {
     repo: 'Lanlanb/my-notion',
